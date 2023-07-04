ca8bc7deb03c25cbbc52ac01159282895eda38a6
Zweird-958 <xdfamilyytt@gmail.com>
2023-07-04 09:16:14 +0200

    feat: add sign in page

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,18 +1,16 @@
-import Label from "@/web/components/Label";
 import clsx from "clsx";
 import { ErrorMessage, Field as FieldFormik } from "formik";
 
 const Field = (props) => {
-	const { name, placeholder, className, label, ...otherProps } = props;
+	const { name, placeholder, className, ...otherProps } = props;
 
 	return (
-		<div className="mx-auto w-3/4">
-			<Label>{label}</Label>
+		<div>
 			<FieldFormik
 				name={name}
 				placeholder={placeholder}
 				className={clsx(
-					"w-full rounded border-2 border-blue-400 bg-transparent px-2 py-1 focus:border-blue-600 dark:text-white",
+					"w-full rounded border-2 border-blue-400 bg-transparent px-2 py-1 focus:border-blue-600",
 					className
 				)}
 				{...otherProps}
--- src/components/Form.js
+++ src/components/Form.js
@@ -9,11 +9,11 @@ const Form = (props) => {
 			<FormFormik
 				noValidate
 				className={clsx(
-					"mx-auto my-5 flex w-1/2 flex-col gap-4 rounded border-2 border-blue-400 px-2 py-6 shadow-lg shadow-blue-400 dark:border-blue-600 dark:shadow-blue-600 md:w-2/5 lg:w-1/4",
+					"mx-auto my-5 flex flex-col gap-4 rounded border-2 border-blue-400 py-6 shadow-lg shadow-blue-400 px-10",
 					className
 				)}
 			>
-				<p className="mx-auto w-fit text-3xl font-bold tracking-wider text-blue-400 transition-all hover:scale-110 dark:text-blue-600">
+				<p className="mx-auto w-fit text-3xl font-bold tracking-wider text-blue-400 transition-all hover:scale-110">
 					{title}
 				</p>
 				{children}
