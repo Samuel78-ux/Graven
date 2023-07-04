3efe6d94e4788077aedf11aa5d88e64490e92f64
Zweird-958 <xdfamilyytt@gmail.com>
2023-07-04 11:35:59 +0200

    feat: add appbar

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,5 +1,6 @@
 import Field from "@/components/Field";
 import Form from "@/components/Form";
+import Page from "@/components/Page";
 
 const initialValues = {
 	firstname: "",
@@ -15,30 +16,32 @@ const Register = () => {
 	};
 
 	return (
-		<div className="flex justify-center items-center h-screen">
-			<Form
-				title="Register"
-				initialValues={initialValues}
-				onSubmit={handleSubmit}
-				className="w-72"
-			>
-				<Field name="firstname" placeholder="Prénom" />
-				<Field name="lastname" placeholder="Nom" />
-				<Field name="email" placeholder="Email" />
-				<Field name="password" placeholder="Mot de passe" type="password" />
-				<Field
-					name="confirmPassword"
-					placeholder="Confirmer le mot de passe"
-					type="password"
-				/>
-				<button
-					type="submit"
-					className="w-full bg-blue-400 text-white font-bold rounded px-2 py-1 hover:bg-blue-600 transition-all"
+		<Page>
+			<div className="flex justify-center items-center h-screen absolute w-screen -z-10">
+				<Form
+					title="Register"
+					initialValues={initialValues}
+					onSubmit={handleSubmit}
+					className="w-72"
 				>
-					S'inscrire
-				</button>
-			</Form>
-		</div>
+					<Field name="firstname" placeholder="Prénom" />
+					<Field name="lastname" placeholder="Nom" />
+					<Field name="email" placeholder="Email" />
+					<Field name="password" placeholder="Mot de passe" type="password" />
+					<Field
+						name="confirmPassword"
+						placeholder="Confirmer le mot de passe"
+						type="password"
+					/>
+					<button
+						type="submit"
+						className="w-full bg-blue-400 text-white font-bold rounded px-2 py-1 hover:bg-blue-600 transition-all"
+					>
+						S'inscrire
+					</button>
+				</Form>
+			</div>
+		</Page>
 	);
 };
 
