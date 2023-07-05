4cb4456a05ed4c81df7e40dfb050aabcda015230
HAMEL Julien <julienhamel.h@gmail.com>
2023-07-05 22:01:22 +0200

    feat: add project

---
+++ src/api/middlewares/auth.js
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client";
+import config from "../config.js";
+import jsonwebtoken from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+const auth = async (req, res, next) => {
+	const { authorization } = req.headers;
+
+	if (!authorization) {
+		res.status(403).send({ error: "Forbidden" });
+
+		return;
+	}
+
+	try {
+		const { payload } = jsonwebtoken.verify(
+			authorization,
+			config.security.jwt.secret
+		);
+
+		const user = await prisma.user.findUnique({
+			where: {
+				id: payload.userId,
+			},
+		});
+
+		if (!user) {
+			res.status(403).send({ error: "Forbidden" });
+
+			return;
+		}
+
+		req.user = user;
+
+		await next();
+	} catch (err) {
+		if (err instanceof jsonwebtoken.JsonWebTokenError) {
+			res.status(403).send({ error: "Forbidden" });
+
+			return;
+		}
+
+		console.error(err);
+
+		res.status(500).send({ error: "Oops. Something wrong." });
+	}
+};
+
+export default auth;
---
+++ src/pages/api/project.js
@@ -0,0 +1,54 @@
+import auth from "@/api/middlewares/auth";
+import mw from "@/api/utils/mw";
+import { PrismaClient } from "@prisma/client";
+import multer from "multer";
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/images"); // Répertoire de destination des images
+	},
+	filename: (req, file, cb) => {
+		const fileName = `${Date.now()}-${file.originalname}`; // Nom de fichier personnalisé
+		cb(null, fileName);
+	},
+});
+
+const upload = multer({ storage });
+
+const prisma = new PrismaClient();
+
+const handler = mw({
+	POST: [
+		auth,
+		upload.single("image"),
+		async (req, res) => {
+			const { file, body, user } = req;
+			const { name, description, price, duration } = body;
+
+			try {
+				const project = await prisma.project.create({
+					data: {
+						name,
+						description,
+						price: parseInt(price),
+						duration: parseInt(duration),
+						image: file.filename,
+						userId: user.id,
+					},
+				});
+				res.send({ result: project });
+			} catch (error) {
+				console.log(error);
+				res.status(500).send({ error: "Something went wrong." });
+			}
+		},
+	],
+});
+
+export default handler;
