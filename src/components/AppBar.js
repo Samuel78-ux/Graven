9e1c4a53527a7500a7c59667436d67b7e21ca84d
Zweird-958 <xdfamilyytt@gmail.com>
2023-07-05 11:11:30 +0200

    feat: add icon on appbar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,11 +1,16 @@
 import LinkAppBar from "@/components/LinkAppBar";
 import Link from "next/link";
+import { HomeModernIcon } from "@heroicons/react/24/outline";
 
 const AppBar = () => {
 	return (
 		<div className="flex shadow-lg p-4 justify-between">
-			<Link href="/" className="p-2 font-bold">
-				Accueil
+			<Link
+				href="/"
+				className="p-2 font-bold flex gap-2 text-xl items-center justify-center"
+			>
+				<HomeModernIcon className="w-6 h-6" />
+				JedImmo
 			</Link>
 			<div className="flex gap-4 mr-5">
 				<LinkAppBar className="bg-green-500 p-2" href="/sign-in">
