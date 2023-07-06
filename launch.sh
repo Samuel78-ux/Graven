d39e7888293155e9e48de64e9c29d81dba1a3a6c
padac78 <72388269+padac78@users.noreply.github.com>
2023-07-06 14:27:13 +0200

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Commande git pull
+git pull
+# Commande npm run sync
+npm run sync
+# Commande docker build
+docker build -t docker_nextjs:development .
+# Commande docker run en mode détaché
+docker run --publish 3000:3000 --detach docker_nextjs:development
+ 
