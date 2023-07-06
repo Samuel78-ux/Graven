20bb3cab35a436bafdf77538937a43ebb3acb358
Zweird-958 <xdfamilyytt@gmail.com>
2023-07-06 09:21:55 +0200

    chore: uninstall nextUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { nextui } = require("@nextui-org/react");
+// const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -6,7 +6,6 @@ module.exports = {
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
@@ -18,5 +17,6 @@ module.exports = {
 		},
 	},
 	darkMode: "class",
-	plugins: [nextui()],
+	// plugins: [nextui()],
+	plugins: [],
 };
