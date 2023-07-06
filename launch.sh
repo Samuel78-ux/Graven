041d13e1ac14a69fb787b0d765f8f6436e0aa561
pierrealexandredaccord7@gmail.com <pierrealexandredaccord7@gmail.com>
2023-07-06 15:42:58 +0200

    commit

--- launch.sh
+++ launch.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Commande git pull
 git pull
-#install modules
-npm i
 # Commande npm run sync
 npm run sync
 # Commande docker build
 docker build -t docker_nextjs:development .
 # Commande docker run en mode détaché
 docker run --publish 3000:3000 --detach docker_nextjs:development
+
 docker ps
